f0a2310e17a3c6be4f6045a08f0926a06b214289
Dennis <dennismusembi344@gmail.com>
2024-09-12 13:19:33 +0300

    redone

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // Write your code in this file!
-const currentUser= "Dennis Musembi"
-const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`
+// const currentUser= "Dennis Musembi"
+// const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`
+// const excitedWelcomeMessage=welcomeMessage.toUpperCase()
+// const shortGreeting=`Welcome, ${currentUser.slice(0,1)}!`
+
+const currentUser="Bancy Malikern"
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`        
 const excitedWelcomeMessage=welcomeMessage.toUpperCase()
-const shortGreeting=`Welcome, ${currentUser.slice(0,1)}!`
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
 
 
 
