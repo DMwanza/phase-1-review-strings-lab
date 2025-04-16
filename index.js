b1c1c32cb8b8ad461aa1ab428c8cd04bd6701093
+ <+>
2025-04-16 16:10:19 +0300

    new done

--- index.js
+++ index.js
@@ -4,10 +4,17 @@
 // const excitedWelcomeMessage=welcomeMessage.toUpperCase()
 // const shortGreeting=`Welcome, ${currentUser.slice(0,1)}!`
 
-const currentUser="Bancy Malikern"
-const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`        
+// const currentUser="Bancy Malikern"
+// const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`        
+// const excitedWelcomeMessage=welcomeMessage.toUpperCase()
+// const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
+const currentUser='Grace Hopper'
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`
 const excitedWelcomeMessage=welcomeMessage.toUpperCase()
-const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+const shortGreeting=`Welcome, ${currentUser.slice(0,1)}!`
+
 
 
 
